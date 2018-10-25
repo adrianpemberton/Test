21f982bb811b9b7b43b428f08c22534b5fdae4a8
adrianpemberton <adrian@alfutures.co.uk>
2018-10-25 15:16:09 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ else
     for f in $filesChanged
 	do
 		#do not upload these files that aren't necessary to the site
-		if [ "$f" != ".travis.yml" ] && [ "$f" != "deploy.sh" ]
+		if [ "$f" != ".travis.yml" ] && [ "$f" != "deploy.sh" ] && [ "$f" != "README.md" ]
 		then
 	 		echo "Uploading $f"
 	 		curl --ftp-create-dirs -T $f -u $FTP_USER:$FTP_PASSWORD ftp://travis.alfutures.co.uk/$f
